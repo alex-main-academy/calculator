e8c5c5ffffc3c54676c9685e8a1b787809fe5fea
Alex <oleksandrilnytskyi@gmail.com>
2021-12-02 19:07:21 +0200

    commit

---
+++ main.js
@@ -0,0 +1,49 @@
+let input = document.querySelector('.calculator__input');
+let inputArr = [];
+
+let buttonNum = document.querySelectorAll('.button__darkgray');
+let buttonSym = document.querySelectorAll('.button__orange')
+let buttonClear = document.querySelector('.button__clear');
+let buttonResult = document.querySelector('.button__result')
+
+
+buttonNum.forEach((elem) => {
+    elem.addEventListener('click', () => {
+        inputArr.push(elem.innerHTML)
+        input.innerHTML = inputArr.join('');
+
+        if (inputArr[0] == 0) {
+            inputArr.splice(0, inputArr.length)
+        }
+
+        if (inputArr[0] == buttonNum[10].innerHTML) {
+            inputArr.splice(0, inputArr.length)
+            input.innerHTML = inputArr.join('')
+        }
+    })
+})
+
+
+buttonSym.forEach((elem) => {
+    elem.addEventListener('click', () => {
+        if (inputArr[0] == this.elem) {
+            inputArr.splice(0, inputArr.length)
+        }else {
+            inputArr.push(elem.innerHTML)
+            input.innerHTML = inputArr.join('')
+        }
+
+    })
+})
+
+
+buttonResult.addEventListener('click', () => {
+    
+})
+
+
+buttonClear.addEventListener('click', () => {
+    inputArr.splice(0, inputArr.length)
+    input.innerHTML = inputArr.join('')
+})
+
